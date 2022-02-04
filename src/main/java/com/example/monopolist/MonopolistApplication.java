8ce9e4061d9f3c50df3c3ca97e5d62489fc4c07f
Chinami <l.vong@hotmail.fr>
2022-02-04 09:57:50 +0100

    first commit

---
+++ src/main/java/com/example/monopolist/MonopolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.monopolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monopolist/MonopolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monopolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
